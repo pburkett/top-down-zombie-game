738303dbf21f72c3c3a4bf5f2c7fe7cda0c5105d
pburkett <pburkett636@gmail.com>
2022-05-01 12:55:31 -0600

    changes

--- Level.gd
+++ Level.gd
@@ -6,18 +6,6 @@ onready var nav_2d = get_node("/root/Main/Level/Navigation")
 
 func _ready():
 	pass
-	
-func _input(ev):
-	if ev is InputEventKey:
-		if ev.scancode == KEY_R:
-			restart_level()
-		elif ev.scancode == KEY_ESCAPE: 
-			get_tree().quit()	
-
-func restart_level():
-	for node in get_tree().get_nodes_in_group("clear_on_level_reload"):
-		node.queue_free()
-	get_tree().reload_current_scene()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
---
+++ Main/Main.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+func _input(ev):
+	var input = Vector2()
+	if Input.is_action_just_released('ui_restart'):
+		restart_level()
+	# TODO remove this when in prod
+	elif ev.scancode == KEY_ESCAPE: 
+		get_tree().quit()	
+
+func restart_level():
+	for node in get_tree().get_nodes_in_group("clear_on_level_reload"):
+		node.queue_free()
+	get_tree().reload_current_scene()
